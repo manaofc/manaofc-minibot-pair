a7bbbff364ec45372de2c36b225519279ff4ec12
manaofc <manisha1718manisha@gmail.com>
2025-11-07 11:29:10 +0530

    Add files via upload

--- pair.js
+++ pair.js
@@ -11,8 +11,31 @@ const {
     Browsers,
     jidNormalizedUser
 } = require("manaofc-baileys");
-const { upload } = require('./mega');
+const mega = require("megajs");
+const auth = {
+    email: 'manisha1718manisha@gmail.com',
+    password: 'manisha2006@ms',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+}
 
+const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) throw err;
+                        storage.close()
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true });
